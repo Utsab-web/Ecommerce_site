c113ccbfb00132debe1c0ffa4cacb27453145091
Utsab Bhusal <utsab0515@gmail.com>
2026-06-11 21:42:14 +0545

    Some changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,22 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
+  const input_icon = "h-[1.3em] w-[1.3em] ";
+
   const link =
     "relative text-white py-2 px-4 after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-[#0f9] after:transition-all hover:after:w-full hover:text-[#0f9]";
 
-  const hamburger_spans = "w-[30px] h-[3px] bg-white transition-all block";
+  const hamburger_spans = "w-[15px] h-[3px] bg-white transition-all block";
 
   const list = "w-full text-center md:w-auto md:text-left";
 
   return (
     <nav className="sticky top-0 px-[5%] w-full bg-[#121212fa] border-b border-[#ffffff1a] flex items-center z-50">
       {/* Logo */}
-      <div className=" mr-auto flex justify-items-start" onClick={() => navigate("/home")}>
+      <div
+        className=" mr-auto flex justify-items-start"
+        onClick={() => navigate("/home")}
+      >
         <p className="text-sm md:text-xl font-bold text-[#0f9] cursor-pointer ">
           Ecommerce Site
         </p>
@@ -33,12 +38,17 @@ function Navbar() {
 
       {/*Search bar*/}
 
-      <div className="flex-1 items-center justify-center md:shrink-0 flex">
+      <div className="flex-1 items-center justify-center md:shrink-0 flex text-sm">
         <input
           type="text"
           placeholder="Search products..."
-          className="wd-[80%] rounded-[25px] p-[0.6em]  w-[60%] border-none outline-none text-white bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5) md:wd-[60%]]"
+          className="w-[90%] rounded-[25px] p-[0.6em] border-none outline-none text-white bg-[#171717] shadow-[inset_2px_5px_10px_rgb(5,5,5)] md:w-[70%]"
         />
+        <svg className={`${input_icon}`} viewBox="0 0 32 32">
+          <g transform="translate(-256.000000, -1139.000000)" fill="#ffffff">
+            <path d="M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z"></path>
+          </g>
+        </svg>
       </div>
 
       {/* Nav Links */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "../components/Navbar";
+
 function Home() {
     return (
         <>
-            <h1 className="text-3xl font-bold text-center mt-[2em]">Welcome to the Home Page</h1>
+            <Navbar/>
         </>
     )
 
